9da64b7b198f2db6adec2c7add507daefcd6897d
李君 <176125727@qq.com>
2017-08-09 09:20:20 +0800

    Update Application.kt

--- src/main/kotlin/org/lijun/Application.kt
+++ src/main/kotlin/org/lijun/Application.kt
@@ -41,6 +41,4 @@ open class Application
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
-
-    println(SpringContextHolder.getBean(DataSource::class.java))
 }
